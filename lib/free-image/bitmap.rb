72bfcb5ededc1b7095d6197c2894366f35975122
Tom Wardrop <tom@tomwardrop.com>
2015-12-01 10:34:27 +1000

    Support for StringIO and Tempfile
    This adds support for StringIO and Tempfile from the standard library. Is dynamic so StringIO and Tempfile don't need to be included by FreeImage.

--- lib/free-image/bitmap.rb
+++ lib/free-image/bitmap.rb
@@ -166,7 +166,7 @@ module FreeImage
       case source
       when AbstractSource
         source
-      when ::IO
+      when ::IO, (const_get(:StringIO) rescue ::IO), (const_get(:Tempfile) rescue ::IO)
         IO.new(source)
       when String
         File.new(source)
